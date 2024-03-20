200d6ce7aacc0cb0bba2874a45297ad90026a710
kate-pla <115516204+kate-pla@users.noreply.github.com>
2024-03-19 19:44:15 -0700

    Add files via upload

---
+++ MarkovLinkedList.cs
@@ -0,0 +1,67 @@
+﻿
+using MyLinkedListLibrary;
+public class MarkovEntry
+{
+    private SortedList<string, int> nextWords;
+    private string root;
+    private int count;
+    private Random rng;
+
+    public MarkovEntry(string root)
+    {
+        this.root = root;
+        count = 0;
+        nextWords = new SortedList<string, int>();
+        rng = new Random();
+    }
+
+    public void Add(string str)
+    {
+        if (nextWords.ContainsKey(str))
+        {
+            nextWords[str]++;
+        }
+        else
+        {
+            nextWords.Add(str, 1);
+        }
+        count++;
+    }
+
+    public string RandomWord()
+    {
+        int index = rng.Next(nextWords.Count);
+        return nextWords.Keys.ElementAt(index);
+    }
+
+    public string WeightedRandomWord()
+    {
+        List<string> options = new List<string>();
+
+        foreach (KeyValuePair<string, int> pair in nextWords)
+        {
+            for (int x = 0; x < pair.Value; x++)
+            {
+                options.Add(pair.Key);
+            }
+        }
+
+        int index = rng.Next(options.Count);
+
+        return options[index];
+    }
+
+    public override string ToString()
+    {
+        string output = $"'{root}' ({count}) : ";
+
+        foreach (KeyValuePair<string, int> pair in nextWords)
+        {
+            output += $"'{pair.Key}' {pair.Value}, ";
+        }
+
+        return output;
+    }
+
+
+}
---
+++ MarkovLinkedListTest.cs
@@ -0,0 +1,117 @@
+﻿
+/*
+* Markov Text Generation 
+* Create a story
+*
+*file: MarkovLinkedList.cs
+* author: Dylan Fox, Teagan Algarra, Madeline Stokes, Kate Pla
+*/
+
+using MyLinkedListLibrary;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System;
+
+public class MarkovLinkedListTest
+{
+    
+    static void Main(string[] args)
+    {
+        // failsafe if number of arguments is wrong
+        if (args.Length < 1 || args.Length > 3)
+        {
+            Console.WriteLine("Incorrect number of arguments! Please try again with the following format: ");
+            Console.WriteLine("{text file} {markov degree} {output length}");
+            return;
+        }
+        Stopwatch stopwatch = new Stopwatch();
+
+        // starts timer
+        stopwatch.Start();
+
+        // splits text into single word array
+        string[] splitText = File.ReadAllText(args[0]).Split(" ");
+
+        int markovDegree = int.Parse(args[1]);
+
+        int wordLength = int.Parse(args[2]);
+
+        SortedList<string, MarkovEntry> testDict = new SortedList<string, MarkovEntry>();
+
+        // iterates through array, adding to dictionary each time
+        for (int x = 0; x < splitText.Length - markovDegree; x++)
+        {
+            string result = String.Join(" ", splitText, x, markovDegree);
+
+            if (testDict.ContainsKey(result))
+            {
+                // accesses the MarkovEntry.Add() function
+                testDict[result].Add(splitText[x + 2]);
+            }
+            else
+            {
+                testDict.Add(result, new MarkovEntry(result));
+                testDict[result].Add(splitText[x + 2]);
+            }
+        }
+
+        // seed text
+        string output = "I don't";
+
+        for (int i = 2; i < wordLength; i++)
+        {
+            string[] words = output.Split(' ');
+            string key = words[i - 2] + " " + words[i - 1];
+
+            // failsafe for when key does not exist
+            while (!testDict.ContainsKey(key))
+            {
+                key = RandomKey();
+            }
+
+            string randomValue = testDict[key].WeightedRandomWord();
+            output += " " + randomValue;
+
+        }
+
+        // to make sure that the output ends in a period
+        int y = 2;
+
+        while (!output.EndsWith("."))
+        {
+            string[] words = output.Split(' ');
+            string key = words[y - 2] + " " + words[y - 1];
+
+            // failsafe for when key does not exist
+            while (!testDict.ContainsKey(key))
+            {
+                key = RandomKey();
+            }
+
+            string randomValue = testDict[key].WeightedRandomWord();
+            output += " " + randomValue;
+            y++;
+        }
+
+        Console.WriteLine(output);
+        Console.WriteLine("");
+
+        // stop stopwatch and print the milliseconds
+        stopwatch.Stop();
+        Console.WriteLine($"The code took {stopwatch.ElapsedMilliseconds} milliseconds or {stopwatch.Elapsed} seconds to execute.");
+
+        // size of file
+        long length = new FileInfo(args[0]).Length;
+        long kb = length / 1000;
+
+        Console.WriteLine($"The size of the file was {length} bytes or {kb} kB.");
+        // failsafe that selects a random key in case one does not exist
+        string RandomKey()
+        {
+            Random rng = new Random();
+            int index = rng.Next(testDict.Count);
+            return testDict.Keys.ElementAt(index);
+        }
+    }
+}
